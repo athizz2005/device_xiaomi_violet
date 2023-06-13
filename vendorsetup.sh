9bbbade214137bab1aa073afc80d46bcaed7eecb
JodOrWot <111036482+athizz2005@users.noreply.github.com>
2023-06-13 09:59:37 +0530

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_violet -b eleven vendor/xiaomi/violet
+git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_violet -b LA.UM.9.1.r1-11100-SMxxx0.0 kernel/xiaomi/violet
+git clone https://gitlab.com/Joker-V2/android_vendor_MiuiCamera -b eleven vendor/ANXCamera
